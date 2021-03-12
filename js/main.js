59c040b9a17815373939c2008df0e24e0fd4ed05
Isrrael <israeldtfc1974@gmail.com>
2021-03-12 21:01:24 +0100

    Maquetacion del HTML de contienda, inicio de codigo js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-let selectionA = document.querySelector(".cntPa1 cntPa2 > .pig1 .pig2 .pig3 .pig4")
-.addEventListener("click", () =>{
-    return selectionA
-})
+let dic1 = document.getElementsByClassName("pig1");
+let dic2 = document.getElementsByClassName("pig2");
+let dic3 = document.getElementsByClassName("pig3");
+let dic4 = document.getElementsByClassName("pig4");
+
+let dictators = [dic1, dic2, dic3, dic4];
+
+console.log(dictators[1]);
+
