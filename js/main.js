800914ded941a4df75587011113a3c02db8ba717
Isrrael <israeldtfc1974@gmail.com>
2021-03-13 09:31:47 +0100

    desarrollo de posibles soluciones en el segmento de seleccion

--- js/main.js
+++ js/main.js
@@ -5,5 +5,38 @@ let dic4 = document.getElementsByClassName("pig4");
 
 let dictators = [dic1, dic2, dic3, dic4];
 
-console.log(dictators[1]);
+// posible solucion con forEach
+/*
+document.querySelectorAll('dictators').forEach(item => {
+    item.addEventsListener('dbclick', () => {
+        return document.getElementById("slcA").innerHTML = selA;
+    })
+})*/
 
+// no me funciona :(
+
+//----------------------------------------------------
+// segundo intento con forEach
+
+document.addEventListener('dblclick', function () {
+        let selA = dictators[dictators.forEach];
+        return document.getElementById("slcA").innerHTML = selA;
+    })
+
+//da como resultado undifined :(
+
+//------------------------------------------------------
+
+// posible solucion con un bucle for
+
+// cada elemento tendra mediante un bucle for la funcion onclick
+/*
+for (let i = 0; i < dictators.length; i++){
+    
+    dictators[i].addEventListener('dbclick', () => {
+        console.log(i)
+    })
+
+}
+
+document.getElementById("slcA").innerHTML = selA;*/
